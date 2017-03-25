8b6bedb333c77838260c49cd9db10149a7ffa65e
Robbie Dozier <importantigrav@gmail.com>
2017-03-25 06:38:10 -0400

    Added map view

--- GeoBoard/ViewController.swift
+++ GeoBoard/ViewController.swift
@@ -10,17 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var newAccountButton: UIButton!
     @IBOutlet weak var BackgroundImageView: UIImageView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    var effect:UIVisualEffect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.black
         addParallaxToView(vw: BackgroundImageView)
+        effect = blurView.effect
+        blurView.effect = nil
+        appTitle.alpha = 0
+        emailField.alpha = 0
+        passwordField.alpha = 0
+        logInButton.alpha = 0
+        newAccountButton.alpha = 0
+        
+        // Animate in
+        UIView.animate(withDuration: 1) {
+            self.blurView.effect = self.effect
+            self.appTitle.alpha = 1
+            self.emailField.alpha = 1
+            self.passwordField.alpha = 1
+            self.logInButton.alpha = 1
+            self.newAccountButton.alpha = 1
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logInWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func newAccountWasPressed(_ sender: Any) {
     }
     
     func addParallaxToView(vw: UIView) {
