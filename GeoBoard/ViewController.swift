20693b274280507962c28d9ce99c6b72e5ef2765
Robbie Dozier <importantigrav@gmail.com>
2017-03-25 04:58:39 -0400

    Initial Commit

---
+++ GeoBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoBoard
+//
+//  Created by Robbie Dozier on 3/25/17.
+//  Copyright © 2017 HuskyDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
